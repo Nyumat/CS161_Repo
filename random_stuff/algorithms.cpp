5aa0eb588b56b42961e27e0ea5130e68c2c70cb7
thomas <46255836+Nyumat@users.noreply.github.com>
2021-03-03 00:13:12 -0500

    Update algorithms.cpp

--- random_stuff/algorithms.cpp
+++ random_stuff/algorithms.cpp
@@ -1,15 +1,1 @@
-
-using namespace std;
-
-
-int main(short int rows, int columns) {
-
-
-      int arr[rows][columns] = 0
-
-      for (i=0;i<10;i++){
-            int rows.append("*")
-
-      }
-
-}
+// Use this for leetcode practice in C++.
