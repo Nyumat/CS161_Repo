e0e8c7a2a00f158a7d7c78d77973400b65833fb1
thomas <46255836+Nyumat@users.noreply.github.com>
2021-03-05 13:21:22 -0500

    Added Binary Search

--- random_stuff/algorithms.cpp
+++ random_stuff/algorithms.cpp
@@ -1,1 +1,45 @@
 // Use this for leetcode practice in C++.
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// Recursive Binary Search is O(logn) because our search is shrinking in half each time.
+
+int binary_Search(int arr[], int left, int right, int target) {
+
+    if (left <= right) {
+
+        int mid = (left + right) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        if (arr[mid] > target)
+            return binary_Search(arr,left,right - 1, target);
+        if (arr[mid] < target)
+            return binary_Search(arr,mid + 1, right, target);
+
+    }
+    return -1;
+}
+
+int main() {
+
+    int arr[] = {1,4,15,20,22,25,33,36,41};
+
+    int n = sizeof(arr) / sizeof (arr[0]);
+
+    int target = 36;
+
+    int index = binary_Search(arr,0,n-1,target);
+
+    if (index == -1){
+        cout << target << " was not present in the array. ";
+    } else {
+        cout << target << " was found at index " << index << " in the array.";
+    }
+
+    return 0;
+
+}
